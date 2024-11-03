4cc690e70efea599d304c9c64281979a974ffaa6
Muhammed Zohaib Anwer <zohaibanwer2002@gmail.com>
2024-11-03 07:13:48 +0500

    created flutter project

---
+++ android/app/src/main/kotlin/com/example/fetching_api_flutter_with_search/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fetching_api_flutter_with_search
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("USER PROFILES"),
+        ),
+        body: const Center(
+          child: Text("Hello World"),
+        ),
+      ),
+    );
+  }
+}
