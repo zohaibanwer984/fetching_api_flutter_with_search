42c9ea8938fc2c669836ce67bda9f76e31774671
Muhammed Zohaib Anwer <zohaibanwer2002@gmail.com>
2024-11-03 08:21:00 +0500

    implemend UI in listView

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:fetching_api_flutter_with_search/api_service.dart';
+import 'package:fetching_api_flutter_with_search/profile_card.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -10,16 +12,45 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  List<dynamic> _userIds = [];
+
+  Future<void> getUserIds() async {
+    try {
+      final data = await ApiService.fetchUsers();
+      setState(() {
+        _userIds = data;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getUserIds();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
+          centerTitle: true,
           title: const Text("USER PROFILES"),
         ),
-        body: const Center(
-          child: Text("Hello World"),
-        ),
+        body: (_userIds.isNotEmpty)
+            ? ListView.builder(
+                itemCount: _userIds.length,
+                itemBuilder: (context, index) {
+                  final user = _userIds[index];
+                  return ProfileCard(user: user);
+                },
+              )
+            : const Center(
+                child: CircularProgressIndicator(),
+              ),
       ),
     );
   }
